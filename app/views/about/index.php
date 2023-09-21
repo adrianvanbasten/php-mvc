b6c382d3b1b3afdac1216189439876d684746fb6
adriandejong <syah100629@gmail.com>
2023-09-21 09:51:02 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,21 @@
+<body class="bg-dark">
+<div class="container mt-4">
+    <div class="d-flex flex-column align-items-center gap-4">
+        <h1 class="text-white">About Me</h1>
+        <img src="<?= BASEURL; ?>/img/adrian.jpg" class="rounded-circle shadow-lg" width="200">
+        <p class="text-white">Hello, my name is <?= $data['nama']; ?>, I a <?= $data['status'] ; ?> at SMK NEGERI 40 JAKARTA, i was <?= $data['umur']; ?> years old. </p>
+        <p class="text-white">
+I'm a passionate individual on a journey to fulfill my dreams and aspirations. Life to me is an ever-evolving canvas filled with unique experiences and adventures. I'm an avid lover of art and see life as a work of art constantly in progress.
+
+As a true book enthusiast, I'm always seeking wisdom and new knowledge within the pages of books. Nature is my sanctuary, and I find peace and inspiration in exploring the great outdoors.
+
+I'm a lifelong learner, constantly striving for personal growth and development. I believe that happiness lies in simplicity, and I aim to live a humble and purposeful life.
+
+Being a good listener, I enjoy connecting with new people and learning from their stories. I have big dreams and a strong determination to achieve them, as I believe that limits exist only in our minds.
+
+Family means the world to me, and I aspire to be a positive influence in their lives.
+
+Let's connect and share our journeys!</p>s
+    </div>
+</div>
+</body>
---
+++ app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container mt-4 bg-transparent">
+    <div class="text-white">
+        <h1 class="text-center"><strong> selamat datang di Situs Adrian Van Basten</strong></h1>
+        <p class="lead text-center">Hallo, perkenalkan nama saya <?= $data['nama']; ?></p>
+        <hr class="my-4">
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nam dolores voluptatum fugit vitae officia numquam ex quasi praesentium est, atque, porro dolore iure beatae cupiditate quos? Voluptatibus amet dolore ducimus aspernatur explicabo odit officia.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Pelajari lebih lanjut</a></p>
+    </div>
+</div>
